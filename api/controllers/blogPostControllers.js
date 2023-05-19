568e3794481c580c0d65a01acb85938852a6d4eb
Chirag <chiragmakwana1807@gmail.com>
2023-05-19 14:17:38 +0530

    get post done with liked or not

--- api/controllers/blogPostControllers.js
+++ api/controllers/blogPostControllers.js
@@ -1,7 +1,8 @@
 const blogPostModel = require('../models/blogPostModel');
 const cloudinary = require("cloudinary").v2;
 const fs = require("fs");
-const verifyToken = require('../middlewares/verifyToken');
+const jwt = require('jsonwebtoken');
+const secret_key_Access_Token = process.env.secret_key_Access_Token;
 
 cloudinary.config({
     cloud_name: process.env.cloud_name, 
@@ -14,18 +15,32 @@ const getAllPosts = (req, res) => {
         const headers = req.headers;
         try{
             const result = await blogPostModel.find({author: headers.uid});
-                if(result[0] == undefined){
+                if(result == undefined){
                     res.status(204).json({
                         msg: "No Blog Post Available for this User"
                     })
                 }
                 else {
+                    const modifiedResult = result.map((document) => {
+                        const modifiedDocument = JSON.parse(JSON.stringify(document));
+
+                        if(modifiedDocument.likes.includes(headers.uid)){
+                            modifiedDocument.likedOrNot = 1;
+                        }
+                        else{
+                            modifiedDocument.likedOrNot = 0;
+                        }
+
+                        return modifiedDocument;
+                    })  
+
                     res.status(200).json({
-                    result: result[0],
+                    result: modifiedResult,
                     msg: "User's all Posts Fetched Successfully"
                 })
             }
         } catch (error) {
+            console.log(error);
             res.status(400).json({
                 msg: "Can't fetch User Details"
             })
@@ -45,10 +60,40 @@ const getParticularPosts = (req, res) => {
                 })
             }
             else{
-                res.status(200).json({
-                    result: result[0],
-                    msg: "Post Fetched Successfully"
-                })
+                const accessToken = req.headers.authorization.split(" ")[1];
+                const result1 = jwt.verify(accessToken, secret_key_Access_Token);
+                if(result1 != null){
+                    const modifiedResult = JSON.parse(JSON.stringify(result[0]));
+                    if(modifiedResult.likes.includes(result1.uid)){
+                        modifiedResult.likedOrNot = 1;
+                    }
+                    else{
+                        modifiedResult.likedOrNot = 0;
+                    }
+                    res.status(200).json({
+                        result: modifiedResult,
+                        msg: "Post Fetched Successfully"
+                    })
+                    // if(result[0].likes.includes(result1.uid)){
+                    //     res.status(200).json({
+                    //         result: result[0],
+                    //         likedOrNot: 1,
+                    //         msg: "Post Fetched Successfully"
+                    //     })
+                    // }
+                    // else{
+                    //     res.status(200).json({
+                    //         result: result[0],
+                    //         likedOrNot: 0,
+                    //         msg: "Post Fetched Successfully"
+                    //     })
+                    // }
+                }
+                else{
+                    return res.status(401).json({
+                        msg:"Token is invalid"
+                    })
+                }
             }
         } catch (error) {
             res.status(400).json({
